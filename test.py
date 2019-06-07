26a55c3a86925e9eac2f2370b40ee514d0cda4a3
Philip Slisenko <slisenkophilip@gmail.com>
2019-06-07 12:45:44 +0200

    Added different handlers for messages/commands and inline queries

--- test.py
+++ test.py
@@ -9,20 +9,38 @@ url = "https://api.telegram.org/bot867306259:AAFkrgtHGExlWrtjRrOSHzM-4frKNESxL1w
 
 app = Flask(__name__)
 
+def save_json(obj):
+    with open('json_response.json', 'w') as f:
+        f.write(json.dumps(obj, ensure_ascii=False, indent=4))
+
 
 def process_message(msg):
+    print('processing msg')
     chat_id = msg['message']['chat']['id']
     text = msg['message']['text']
     if text == "/start":
         payload = {'chat_id': chat_id, 'text': f"Hello {msg['message']['from']['first_name']}"}
         requests.post(url + 'sendMessage', json=payload)
 
+
+def process_query(query):
+    print('processing query')
+    pass
+
+
+def process_request(telegram_request):
+    if 'message' in telegram_request:
+        return process_message(telegram_request)
+    if 'inline_query' in telegram_request:
+        return process_query(telegram_request)
+
+
 @app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        msg = request.get_json()
-        process_message(msg)
-        print(json.dumps(msg, indent=4, sort_keys=True))
+        telegram_request = request.get_json()
+        save_json(telegram_request)
+        process_request(telegram_request)
         return Response("Ok", 200)
     else:
         print("test")
